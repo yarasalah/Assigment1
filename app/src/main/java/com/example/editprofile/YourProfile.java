c1fcd5f5cbc59e63976b24d86154ed0f3c5d890b
Yara Almadhoun <yarasalaheddin97@gmail.com>
2019-10-26 00:23:23 +0300

    This is the initial commit

---
+++ app/src/main/java/com/example/editprofile/YourProfile.java
@@ -0,0 +1,14 @@
+package com.example.editprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_profile);
+    }
+}
